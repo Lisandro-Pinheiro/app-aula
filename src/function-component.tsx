0f710b2ff1bffe0af9bb43a2a124d0b758c7f0e6
Lisandro-Pinheiro <lisandropinheiro10@gmail.com>
2023-06-05 21:18:36 -0300

    Commit Inicial

---
+++ src/function-component.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { View, Text, Button } from "react-native";
+
+export default function FunctionComponent (){
+
+    const [contador, setContador] = useState (0);
+    function increment(){
+        setContador(contador +1);
+    }
+    return(
+        <View>
+            <Text>{contador}</Text>
+            <Button title='Clique!' onPress ={()=>{increment();}}/>
+        </View>
+    );
+}
